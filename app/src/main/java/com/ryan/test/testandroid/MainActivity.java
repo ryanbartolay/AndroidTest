74943aa7450b236c17fd0ccce478c18cd89c860c
Ryan <bartolay.ryan@gmail.com>
2017-07-04 15:22:19 +0800

    initial files

---
+++ app/src/main/java/com/ryan/test/testandroid/MainActivity.java
@@ -0,0 +1,26 @@
+package com.ryan.test.testandroid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Hello World", Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+}
